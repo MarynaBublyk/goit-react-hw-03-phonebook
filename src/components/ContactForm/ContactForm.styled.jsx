f18760ff4be8074f133437ca8b4d87d42a0492ec
MarynaBublyk <137985327+MarynaBublyk@users.noreply.github.com>
2024-01-14 22:49:48 +0200

    DZ_3.1

--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -6,7 +6,7 @@ export const Form = styled.form`
   width: 400px;
   padding: 12px;
   background-color: rgba(0, 0, 0, 0.6);
-  border: 3px solid #f08080;
+  border: 3px solid #008000;
   border-radius: 4px;
 `;
 
@@ -14,7 +14,7 @@ export const Label = styled.label`
   display: flex;
   flex-direction: column;
   margin-bottom: 16px;
-  color: #f08080;
+  color: #008000;
   font-size: 20px;
 `;
 
@@ -29,8 +29,8 @@ export const Button = styled.button`
   font: inherit;
   cursor: pointer;
   border-radius: 4px;
-  border: 3px solid #f08080;
-  color: #f08080;
+  border: 3px solid #008000;
+  color: #008000;
   font-size: 20px;
   display: flex;
   align-items: center;
--- src/components/ContactList/ContactList.styled.jsx
+++ src/components/ContactList/ContactList.styled.jsx
@@ -8,13 +8,13 @@ export const List = styled.ul`
   padding: 12px;
   border-radius: 4px;
   background-color: rgba(0, 0, 0, 0.6);
-  border: 3px solid #f08080;
+  border: 3px solid #008000;
   list-style: none;
 `;
 
 export const Item = styled.li`
   background-color: #ffffff;
-  border: 3px solid #f08080;
+  border: 3px solid #008000;
   padding: 10px;
   border-radius: 4px;
   margin-bottom: 5px;
@@ -29,7 +29,7 @@ export const Button = styled.button`
   font: inherit;
   cursor: pointer;
   border-radius: 4px 8px;
-  border: 3px solid #f08080;
+  border: 3px solid #008000;
   display: flex;
   align-items: center;
 `;
