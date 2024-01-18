3afcdab66a6b215196826bcdd4ddac48e0c2ff2e
MarynaBublyk <137985327+MarynaBublyk@users.noreply.github.com>
2024-01-18 23:21:13 +0200

    DZ3

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -48,7 +48,7 @@ class ContactForm extends Component {
             name="name"
             value={this.state.name}
             onChange={this.handleChange}
-            pattern="^[a-zA-Zа-яА-Я]+(([' \\-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            pattern="^[a-zA-Zа-яА-Я\s'\-]+$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
@@ -61,7 +61,7 @@ class ContactForm extends Component {
             name="number"
             value={this.state.number}
             onChange={this.handleChange}
-            pattern="\\+?\\d{1,4}?[ .\\-\\s]?\\(?\\d{1,3}?\\)?[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,9}"
+            pattern="[0-9\-\(\)\+\s]+"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
           />
